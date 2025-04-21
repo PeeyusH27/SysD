eafbb356584c61d42d1d8e8a938e48d7d58a0459
PeeyusH27 <peeyushdas27@gmail.com>
2025-04-21 06:26:18 +0530

    use-noti hook to handle position duration message and type by user

--- toastComponent/src/App.jsx
+++ toastComponent/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import useNotification from './hook/use-notification'
 
 const App = () => {
   //Custom hook for Toast - useNotification(position)
+  const {NotificationComp, triggerNotification} = useNotification('bottom-left')
   return (
-    <div>App</div>
+    <div>
+      {/* <Notification type='success' message={'This is the way!!'}/> */}
+      <button onClick={() => triggerNotification({
+        type:'success',
+        message: 'File Sent Successfully',
+        duration: '3000'
+      })}>Trigger Success</button>
+
+      <button onClick={() => triggerNotification({
+        type:'error',
+        message: 'An Error Occured',
+        duration: '3000'
+      })}>Trigger Error</button>
+      {NotificationComp}
+    </div>
   )
 }
 
